c10103d33ce8eac1156d9ba553be32de24447e61
AmanBhadani <aman.bhadani.01@gmail.com>
2019-10-09 15:18:17 +0530

    commit 2

--- src/main/java/com/stackroute/UserController.java
+++ src/main/java/com/stackroute/UserController.java
@@ -4,19 +4,29 @@ package com.stackroute;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
 
     @RequestMapping("/")
-    public ModelAndView getUserName()
+    public String homePage()
     {
-        System.out.println("inside method");
-        User user = new User("Aman");
-        ModelAndView modelAndView = new ModelAndView("index");
+        return "index";
+    }
+    @RequestMapping("/show")
+    public ModelAndView getUserName(HttpServletRequest httpServletRequest)
+    {
+        String username=(httpServletRequest.getParameter("username"));
+        String password=(httpServletRequest.getParameter("password"));
+        User user=  new User();
+        user.setName(username);
+        user.setPassword(password);
+        ModelAndView modelAndView = new ModelAndView("display");
         modelAndView.addObject("user",user);
         return modelAndView;
     }
+
 
 
 
